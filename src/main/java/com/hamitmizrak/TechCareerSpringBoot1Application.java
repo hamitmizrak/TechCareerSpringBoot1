12044b64462067dca02a93b1a8db4be02c62434b
hamitmizrak <hamitmizrak@gmail.com>
2022-03-29 22:44:43 +0300

    @SpringBootApplication ==> Security (inactived) Excluded

--- src/main/java/com/hamitmizrak/TechCareerSpringBoot1Application.java
+++ src/main/java/com/hamitmizrak/TechCareerSpringBoot1Application.java
@@ -3,7 +3,12 @@ package com.hamitmizrak;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+//exclude:dahil etmemek
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+}
+)
 public class TechCareerSpringBoot1Application {
 
     //psvm
