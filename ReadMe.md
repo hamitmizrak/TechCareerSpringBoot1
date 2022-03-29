eaa1a3c6363af902fd156846f9c4e380bafe04f4
hamitmizrak <hamitmizrak@gmail.com>
2022-03-29 22:18:11 +0300

    Spring Boot Commit First All Project Config

---
+++ ReadMe.md
@@ -0,0 +1,13 @@
+# TeachCareer.NET Spring Boot 
+
+* [GitHub](https://maven.apache.org/guides/index.html)
+
+## Dependency
+
+
+## Project
+
+
+
+
+
---
+++ src/main/java/com/hamitmizrak/TechCareerSpringBoot1Application.java
@@ -0,0 +1,14 @@
+package com.hamitmizrak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechCareerSpringBoot1Application {
+
+    //psvm
+    public static void main(String[] args) {
+        SpringApplication.run(TechCareerSpringBoot1Application.class, args);
+    }
+
+}
