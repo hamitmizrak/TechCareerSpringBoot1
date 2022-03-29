4a2fadd3bdb3af4502a2131c43e4716aea851e5a
hamitmizrak <hamitmizrak@gmail.com>
2022-03-29 23:49:09 +0300

    HomeWork.java

--- src/main/java/com/hamitmizrak/utils/HomeWork.java
+++ src/main/java/com/hamitmizrak/utils/HomeWork.java
@@ -1,4 +1,29 @@
 package com.hamitmizrak.utils;
 
-public class HomeWork {
+public   class HomeWork {
+
+    /*
+    1.DERS
+    -------
+    Git komutlara
+    Linux Basic Komutları cd,mkdir,rm ,cd .
+
+    Clean codes
+    Software Architecture (Yagni,KISS)
+
+    Design pattern yapılara
+    S.O.L.I.D
+
+    Java SE
+    Collection
+    LIST-SET-MAP
+    interface-abstract-inheritance
+    enum
+    seriazalible
+    static
+    inner class
+    Heap memory
+    Stack Memory
+
+     */
 }
