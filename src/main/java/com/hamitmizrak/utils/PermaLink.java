9ca4480d1660d0d7b0fa8d71ac594e00a7671b22
hamitmizrak <hamitmizrak@gmail.com>
2022-03-29 22:49:38 +0300

    utils

---
+++ src/main/java/com/hamitmizrak/utils/PermaLink.java
@@ -0,0 +1,19 @@
+package com.hamitmizrak.utils;
+
+public class PermaLink {
+    //GitHub:  https://github.com/hamitmizrak/TechCareerSpringBoot1
+    //    Emmet site               : https://docs.emmet.io/cheat-sheet/
+//    Visual studio codes      : https://code.visualstudio.com/download
+//    spring initialzr         : https://start.spring.io/
+//    JDK  download            : https://www.oracle.com/java/technologies/javase/jdk17-archive-downloads.html
+//    Git  download            : https://git-scm.com/downloads
+//    ReadMe.md online         : https://dillinger.io/
+//    Ubuntu iso   download    : https://ubuntu.com/download/desktop/thank-you?version=20.04.4&architecture=amd64
+//    Docker                   : https://docs.docker.com/desktop/windows/install/
+//    online Html+css+js       : https://codepen.io/trending
+//    google maps embedded     : https://www.maps.ie/create-google-map/
+//    jsonplaceHolder fake api : https://jsonplaceholder.typicode.com/
+//    font-awesome             : https://fontawesome.com/download
+//    google font              : https://fonts.google.com/
+//    https://www.w3schools.com/js/
+}
---
+++ src/main/java/com/hamitmizrak/utils/ReferancesSites.java
@@ -0,0 +1,10 @@
+package com.hamitmizrak.utils;
+
+public class ReferancesSites {
+
+//    https://www.geeksforgeeks.org/
+//    https://www.hackerrank.com/
+//    LeetCode - The World's Leading Online Programming Learning Platform
+//    https://www.javatpoint.com/java-tutorial
+
+}
