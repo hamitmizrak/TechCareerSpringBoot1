3158d0e8b4c300b1dfefc6499dcac84649b786be
hamitmizrak <hamitmizrak@gmail.com>
2022-03-31 22:01:41 +0300

    JavaSe -1

---
+++ src/main/java/com/hamitmizrak/javase/JavaHomeWork.java
@@ -0,0 +1,14 @@
+package com.hamitmizrak.javase;
+
+public class JavaHomeWork {
+    //API nedir?
+    //IDE nedir ?
+    //compiler nedir ?
+    //interpreter nedir ?
+    //jvm nedir ?
+    //java interpreter ? compiler ?
+    //boxing unboxing
+    //java case sensitivi mi ?
+    //stack memory , heap memory
+    //valueOf ? toString parseInt ?
+}
