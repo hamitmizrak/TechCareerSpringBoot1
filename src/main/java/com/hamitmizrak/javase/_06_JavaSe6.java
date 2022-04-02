58b23015ef222a6f9ffd268f03436119a647a50e
hamitmizrak <hamitmizrak@gmail.com>
2022-04-02 21:14:41 +0300

    JavaSe genel bilgiler

---
+++ src/main/java/com/hamitmizrak/javase/_06_JavaSe6.java
@@ -0,0 +1,36 @@
+package com.hamitmizrak.javase;
+
+public class _06_JavaSe6 {
+
+    public static void main(String[] args) {
+        //cast
+//        float f1 = 34.55f;
+//        long l1 = 451515L;
+//
+//        //boxing
+//        int primitive = 55;
+//        Integer wrapperClass = primitive;
+//
+//        //sayısal ==> String'e çevirmek
+//        int sayi1 = 44;
+//        String kelime = String.valueOf(sayi1);
+//        String kelime2 = Integer.toString(sayi1);
+//        System.out.println(kelime2 + 16);
+//
+//        //String ==> sayısal
+//        String kelime3 = "16";
+//        int sayi3=Integer.parseInt(kelime3);
+//        int sayi4=Integer.valueOf(kelime3);
+//        System.out.println(sayi4+14);
+
+//        //Random
+//        System.out.println(Math.round(Math.random()*4+1));
+//
+//        //object
+//        Random rastgele=new Random();
+//        int sayi=rastgele.nextInt(3)+1;
+//        System.out.println(sayi);
+
+    }
+
+}
