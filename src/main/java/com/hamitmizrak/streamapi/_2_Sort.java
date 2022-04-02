33dca8770310623c6b40b2d7b4f5852e2ef3c98b
hamitmizrak <hamitmizrak@gmail.com>
2022-04-02 22:54:55 +0300

    Stream api (sorted)

---
+++ src/main/java/com/hamitmizrak/streamapi/_2_Sort.java
@@ -0,0 +1,19 @@
+package com.hamitmizrak.streamapi;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class _2_Sort {
+    public static void main(String[] args) {
+        List<String> list=new ArrayList<>();
+        list.add("malatya");
+        list.add("ankara");
+        list.add("denizli");
+
+        List<String> list2=   list.stream().sorted().collect(Collectors.toList());
+        list2.forEach(System.out::println);
+
+    }
+}
