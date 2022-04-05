8018df0bc856aa9ad920121858ec17457262df4b
hamitmizrak <hamitmizrak@gmail.com>
2022-04-05 22:09:20 +0300

    AdminDto

---
+++ src/main/java/com/hamitmizrak/dto/AdminDto.java
@@ -0,0 +1,33 @@
+package com.hamitmizrak.dto;
+
+import lombok.*;
+
+import java.util.Date;
+
+@Getter @Setter
+public class AdminDto extends BaseEntity {
+    private String adminName;
+    private String adminSurname;
+
+    //parametresiz constructo
+    public AdminDto() {
+    }
+
+    //parametreli constructor
+    public AdminDto(String adminId, Date createdDate, String adminName, String adminSurname) {
+        super(adminId, createdDate);
+        this.adminName = adminName;
+        this.adminSurname = adminSurname;
+    }
+
+    @Override
+    public String toString() {
+        return "AdminDto{" +
+                "adminName='" + adminName + '\'' +
+                ", adminSurname='" + adminSurname + '\'' +
+                "} " + super.toString();
+    }
+
+
+
+}
