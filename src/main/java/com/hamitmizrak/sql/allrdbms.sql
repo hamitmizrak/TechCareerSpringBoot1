ad923007522fd63709aa1fca1e5db48403cf3fc7
hamitmizrak <hamitmizrak@gmail.com>
2022-04-05 22:29:01 +0300

    H2Db create table admin or Musql create tanle admin

--- src/main/java/com/hamitmizrak/sql/allrdbms.sql
+++ src/main/java/com/hamitmizrak/sql/allrdbms.sql
@@ -1,3 +1,22 @@
+
+-- Mysql
 DROP DATABASE `techcareer_springboot_1`;
 CREATE SCHEMA `techcareer_springboot_1` DEFAULT CHARACTER SET utf8 COLLATE utf8_turkish_ci ;
 use techcareer_springboot_1;
+
+CREATE TABLE admin(
+"id INT NOT NULL AUTO_INCREMENT,
+"admin_name" VARCHAR(150) NULL,
+"admin_surname" VARCHAR(150) NULL,
+  `created_date` TIMESTAMP NULL DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (`id`));
+
+
+--H2DB
+-- https://www.tutorialspoint.com/h2_database/h2_database_create.htm
+CREATE TABLE admin (
+   id INT NOT NULL,
+   name VARCHAR(150),
+   surname VARCHAR(20) ,
+   create_date DATE
+);
